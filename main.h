51da5441c92fd2f19c4f29311b63dc949c2ffacd
wambugu-jon <mbariwambugu@gmail.com>
2022-04-19 08:21:15 -0700

    Additional includes

--- main.h
+++ main.h
@@ -1,7 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
 
 int _printf(const char *format, ...);
 int _putchar(char c);
